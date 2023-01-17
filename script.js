ec29cd828c88fe9d740664f698bf2257d896fdf4
Berna Yasar <bernayasar@hotmail.co.uk>
2023-01-17 15:37:08 +0000

    added delete button and tried functionality

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const button = document.querySelector("#enter");
 
-//method
+const deleteButton = document.createElement("button");
+
+//method to add item 
 const addItem = (event) => {
 
     // console.dir(event.target.newText.value); 
 
     const toDoItem = document.createElement("li");
+    
     console.dir(event.target)
 
     const inputField = document.querySelector("#new-todo");
@@ -14,7 +17,25 @@ const addItem = (event) => {
 
     const toDoList = document.querySelector("#list");
     toDoList.appendChild(toDoItem);
+    // toDoItem.appendChild(deleteButton);
+
 }
 
 //event
 button.addEventListener('click', addItem);
+
+//method to delete item
+const deleteItem = (event) => {
+    if (deleteButton.onclick === "Delete") {
+    inputField.value.remove();
+  }
+}
+
+deleteButton.removeEventListener("click", deleteItem);
+
+// function delt(event) {
+//     if (button.onclick === "Delete") {
+//     event.target.parentNode.remove();
+//   }
+// }
+
